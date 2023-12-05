fbded25502486cadb6071ba5125593fb3bed883e
Oleksandr Maistrenko <oleksandrmaistrenko100@gmail.com>
2023-12-05 20:09:56 +0100

    Fixed homeWork_two

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Задача 1: За допомогою prompt запитати як звуть користувача. За допомогою alert вивести "Hello, John! How are you?", де John це те, що ввів користувач. Врахувати, що користувач може натиснути cancel або нічого не ввести
 
-// let usersName = prompt("What is your name?", "");
+// const usersName = prompt("What is your name?", "");
 
 // if (usersName) {
 //   alert(`Hello, ${usersName}! How are you?`);
@@ -10,29 +10,27 @@
 
 //Задача 2 : Створити скрипт для складання, віднімання, множення та поділу двох чисел та виведення результатів. Числа вводить користувач, врахувати, що він може ввести невалідні дані
 
-// let enterValueOne = +prompt("Enter first Number ", "");
+// const enterValueOne = +prompt("Enter first Number ", "");
 
-// let enterOperation = prompt("Enter operation ", "");
+// const enterOperation = prompt("Enter operation ", "");
 
-// let enterValueTwo = +prompt("Enter second Number ", "");
+// const enterValueTwo = +prompt("Enter second Number ", "");
 
-// if (isNaN(enterValueOne && enterOperation && enterValueTwo)) {
-//   alert("You should write the numbers");
+// if (
+//   isNaN(enterValueOne || enterOperation || enterValueTwo) ||
+//   enterOperation == "" ||
+//   enterValueTwo == ""
+// ) {
+//   alert("You have wrote something wrong my friend");
 // }
 
-// let sum = enterValueOne + enterValueTwo;
+// const sum = enterValueOne + enterValueTwo;
 
-// let multiplication = enterValueOne * enterValueTwo;
+// const multiplication = enterValueOne * enterValueTwo;
 
-// let difference = enterValueOne - enterValueTwo;
+// const difference = enterValueOne - enterValueTwo;
 
-// function division() {
-//   if (enterValueTwo == 0) {
-//     alert(`This operation is not corect`);
-//   } else {
-//     return enterValueOne / enterValueTwo;
-//   }
-// }
+// const division = enterValueOne / enterValueTwo;
 
 // switch (enterOperation) {
 //   case "+":
@@ -46,24 +44,40 @@
 //     alert(`Result ${multiplication}`);
 //     break;
 //   case "/":
-//     alert(`Result ${division()}`);
-//   default:
+//     alert(`Result ${division}`);
 // }
 
-//Задача 3 Створити скрипт, який отримує від користувача (prompt) два значення і виводить (alert) true, якщо значення рівні, false - якщо ні
+// //Задача 3 Створити скрипт, який отримує від користувача (prompt) два значення і виводить (alert) true, якщо значення рівні, false - якщо ні
 
-// let firstValue = prompt("Enter first Value", "");
+// const firstValue = prompt("Enter first Value", "");
 
-// let secondValue = prompt("Enter second Value", "");
+// const secondValue = prompt("Enter second Value", "");
 
-// firstValue === secondValue ? alert(`true`) : alert(`false`);
+// alert(firstValue === secondValue);
 
-//Задача 4 Розкласти по цифрах п'ятизначне число і вивести у вихідному порядку через пробіл (використовувати оператор “розподіл по модулю” %)
+// //Задача 4 Розкласти по цифрах п'ятизначне число і вивести у виcхідному порядку через пробіл (використовувати оператор “розподіл по модулю” %)
 
-// let enterNumber = prompt("Enter five numbers")
-//   .split("", 5)
-//   .sort((a, b) => a - b);
+let enterNumber = +prompt("Enter five numbers"); // 12345
 
-// alert(enterNumber);
+const array = [];
 
-//Задачу з % не вирiшив на жаль
+if (
+  (isNaN(enterNumber) && -100000 < enterNumber && enterNumber < -9999) ||
+  (9999 < enterNumber && enterNumber < 100000)
+) {
+  while (enterNumber > 0) {
+    let lastNumber = enterNumber % 10;
+
+    array.push(lastNumber); //[5]
+
+    enterNumber = Math.floor(enterNumber / 10); //1234
+
+    array.sort((a, b) => a - b);
+  }
+
+  alert(enterNumber < 0 ? array.join("-") : array.join(" "));
+} else {
+  alert("This value is invalid");
+}
+
+// у 4 зробить, щоб для цілих 5значних (і додатніх, і від'ємних) рахувало і виводило результат, а для нечисел, порожнього значення, нецілих чисел виводило помилку
